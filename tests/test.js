fea7ace13b3195d811de58f6b62eee53238fa52d
AleksNords <arbitalstrike@gmail.com>
2022-12-09 03:41:25 +0100

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected span', async ({ page }) => {
+test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('span')).toBe('Aleks');
+	expect(await page.textContent('h1')).toBeVisible();
 });
