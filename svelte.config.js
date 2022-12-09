1c3a75325484581721670c4a33afb3419f24d0d5
Aleks Nordstrand <arbitalstrike@gmail.com>
2022-12-09 03:30:04 +0000

    Sveltekit changes

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,6 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		vite: {
-			server: { fs: { allow: ['./static'] } }
-		},
 		adapter: adapter({ out: 'build' }),
 	}
 };
