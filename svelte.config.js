20e8e519e3a185b71225c901696515370e65b6ad
AleksNords <arbitalstrike@gmail.com>
2022-12-09 04:03:37 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +6,7 @@ const config = {
 		vite: {
 			server: { fs: { allow: ['./static'] } }
 		},
-		adapter: adapter()
+		adapter: adapter({ out: 'build' }),
 	}
 };
 
