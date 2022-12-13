af9b964c47eecdd183e5d873843dd23b277d3611
AleksNords <arbitalstrike@gmail.com>
2022-12-13 22:40:26 +0100

    Added persistence, updated for newer Sveltekit changes

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,3 +1,10 @@
 import { writable } from 'svelte/store';
+import { browser } from "$app/environment";
 
-export const theme = writable("dark");
+let storedTheme;
+
+if (browser){
+ storedTheme = localStorage.getItem("theme");
+}
+
+export const theme = writable(storedTheme ? storedTheme : "dark");
